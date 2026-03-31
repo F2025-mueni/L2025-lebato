764fa939d1db9a18a18c11c22841dd52afcb9363
F2025-mueni <faithmueni204271@gmail.com>
2026-03-31 05:11:25 -0700

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
 const menu = document.querySelector('#mobile-menu');
 const menuLinks = document.querySelector('.navbar__menu');
 
+// Toggle menu on click
 menu.addEventListener('click', function() {
   menu.classList.toggle('is-active');
   menuLinks.classList.toggle('active');
+});
+
+// Reset menu state on page load
+window.addEventListener('load', () => {
+  menu.classList.remove('is-active');
+  menuLinks.classList.remove('active');
 });
